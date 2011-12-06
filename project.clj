02bcf7ba3e8bff8efe33096639a214b561baa08c
Paul Stadig <paul@stadig.name>
2011-12-06 15:53:53 -0500

    Bump for release of 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus "0.2.0-SNAPSHOT"
+(defproject migratus "0.2.0"
   :description "MIGRATE ALL THE THINGS!"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/java.jdbc "0.1.1"]
