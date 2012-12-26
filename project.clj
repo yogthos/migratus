2f3c604d41de94c3334a181b14ba5badd6508006
Paul Stadig <paul@stadig.name>
2012-12-26 06:42:20 -0500

    Setup lein2.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject migratus "0.5.0"
   :description "MIGRATE ALL THE THINGS!"
+  :aliases {"test!" ["do" "clean," "deps," "test"]}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/java.classpath "0.1.0"]
                  [org.clojure/java.jdbc "0.2.3"]
@@ -12,11 +13,10 @@
                      [log4j "1.2.16"]
                      [mysql/mysql-connector-java "5.1.18"]
                      [robert/bruce "0.7.1"]]
-  :eval-in-project true
-  :migratus {:store :database
-             :migration-dir "migrations/"
-             :db {:classname "com.mysql.jdbc.Driver"
-                  :subprotocol "mysql"
-                  :subname "//localhost/migratus"
-                  :user "root"
-                  :password ""}})
+  :profiles {:dev {:dependencies [[org.clojure/java.classpath "0.1.0"]
+                                  [org.clojure/java.jdbc "0.2.3"]
+                                  [org.clojure/tools.logging "0.2.3"]
+                                  [jar-migrations "1.0.0"]
+                                  [log4j "1.2.16"]
+                                  [mysql/mysql-connector-java "5.1.18"]
+                                  [robert/bruce "0.7.1"]]}})
