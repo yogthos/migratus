82d6382df39012692d60c5dbcc52d52afd2027a8
Yogthos <dmitri.sotnikov@gmail.com>
2015-12-10 00:30:02 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus "0.8.7"
+(defproject migratus "0.8.9"
   :description "MIGRATE ALL THE THINGS!"
   :url "http://github.com/yogthos/migratus"
   :license {:name "Apache License, Version 2.0"
@@ -6,10 +6,10 @@
             :distribution :repo}
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.classpath "0.2.2"]
-                 [org.clojure/java.jdbc "0.3.7"]
+                 [org.clojure/java.classpath "0.2.3"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [camel-snake-kebab "0.3.2"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
                                   [log4j "1.2.17"]
-                                  [com.h2database/h2 "1.4.187"]]}})
+                                  [com.h2database/h2 "1.4.190"]]}})
