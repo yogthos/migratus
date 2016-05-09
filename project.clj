80607fe35816352d94845bee81cbc4d7a77742b2
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-05-09 19:10:12 -0400

    bumped up clojure.java.jdbc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus "0.8.16"
+(defproject migratus "0.8.17"
   :description "MIGRATE ALL THE THINGS!"
   :url "http://github.com/yogthos/migratus"
   :license {:name "Apache License, Version 2.0"
@@ -7,7 +7,7 @@
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.classpath "0.2.3"]
-                 [org.clojure/java.jdbc "0.6.0-rc1"]
+                 [org.clojure/java.jdbc "0.6.0-rc2"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
                                   [log4j "1.2.17"]
