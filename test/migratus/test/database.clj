aeaca9b154020105813e447d38dd635eafa91fc1
Alex Hall <alex.hall@sonian.net>
2017-03-31 15:42:00 +0000

    Test SQL migrations in isolation

--- test/migratus/test/database.clj
+++ test/migratus/test/database.clj
@@ -20,34 +20,13 @@
             [migratus.database :refer :all]
             migratus.logger
             [migratus.test.migration.edn :as test-edn]
+            [migratus.test.migration.sql :as test-sql]
             [migratus.utils :as utils])
   (:import java.io.File))
 
-(def db-store (str (.getName (io/file ".")) "/site.db"))
-
-(def config {:store                :database
-             :migration-dir        "migrations/"
-             :migration-table-name "foo_bar"
-             :db                   {:classname   "org.h2.Driver"
-                                    :subprotocol "h2"
-                                    :subname     db-store}})
-
-(defn reset-db []
-  (letfn [(delete [f]
-            (when (.exists f)
-              (.delete f)))]
-    (delete (io/file "site.db.trace.db"))
-    (delete (io/file "site.db.mv.db"))))
-
-(defn setup-test-db [f]
-  (reset-db)
-  (f))
-
-(defn verify-table-exists? [config table-name]
-  (let [db (connect* (:db config))
-        result (table-exists? db table-name)]
-    (.close (:connection db))
-    result))
+(def config (merge test-sql/test-config
+                   {:store :database
+                    :migration-table-name "foo_bar"}))
 
 (defn test-with-store [store & commands]
   (try
@@ -57,60 +36,60 @@
     (finally
       (proto/disconnect store))))
 
-(use-fixtures :each setup-test-db)
+(use-fixtures :each test-sql/setup-test-db)
 
 (deftest test-make-store
   (testing "should create default table name"
-    (is (not (verify-table-exists?
+    (is (not (test-sql/verify-table-exists?
                (dissoc config :migration-table-name) default-migrations-table)))
     (test-with-store
       (proto/make-store (dissoc config :migration-table-name))
       (fn [config]
-        (is (verify-table-exists? config default-migrations-table)))))
-  (reset-db)
+        (is (test-sql/verify-table-exists? config default-migrations-table)))))
+  (test-sql/reset-db)
   (testing "should create schema_migrations table"
-    (is (not (verify-table-exists? config "foo_bar")))
+    (is (not (test-sql/verify-table-exists? config "foo_bar")))
     (test-with-store
       (proto/make-store config)
       (fn [config]
-        (is (verify-table-exists? config "foo_bar"))))))
+        (is (test-sql/verify-table-exists? config "foo_bar"))))))
 
 (deftest test-init
   (testing "db init"
-    (reset-db)
+    (test-sql/reset-db)
     (let [store (proto/make-store config)]
       (proto/init store))))
 
 (deftest test-migrate
-  (is (not (verify-table-exists? config "foo")))
-  (is (not (verify-table-exists? config "bar")))
-  (is (not (verify-table-exists? config "quux")))
-  (is (not (verify-table-exists? config "quux2")))
+  (is (not (test-sql/verify-table-exists? config "foo")))
+  (is (not (test-sql/verify-table-exists? config "bar")))
+  (is (not (test-sql/verify-table-exists? config "quux")))
+  (is (not (test-sql/verify-table-exists? config "quux2")))
   (core/migrate config)
-  (is (verify-table-exists? config "foo"))
-  (is (verify-table-exists? config "bar"))
-  (is (verify-table-exists? config "quux"))
-  (is (verify-table-exists? config "quux2"))
+  (is (test-sql/verify-table-exists? config "foo"))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (test-sql/verify-table-exists? config "quux"))
+  (is (test-sql/verify-table-exists? config "quux2"))
   (core/down config 20111202110600)
-  (is (not (verify-table-exists? config "foo")))
-  (is (verify-table-exists? config "bar"))
-  (is (verify-table-exists? config "quux"))
-  (is (verify-table-exists? config "quux2"))
+  (is (not (test-sql/verify-table-exists? config "foo")))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (test-sql/verify-table-exists? config "quux"))
+  (is (test-sql/verify-table-exists? config "quux2"))
   (core/migrate config)
-  (is (verify-table-exists? config "foo"))
-  (is (verify-table-exists? config "bar"))
-  (is (verify-table-exists? config "quux"))
-  (is (verify-table-exists? config "quux2"))
+  (is (test-sql/verify-table-exists? config "foo"))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (test-sql/verify-table-exists? config "quux"))
+  (is (test-sql/verify-table-exists? config "quux2"))
   (core/down config 20111202110600 20120827170200)
-  (is (not (verify-table-exists? config "foo")))
-  (is (verify-table-exists? config "bar"))
-  (is (not (verify-table-exists? config "quux")))
-  (is (not (verify-table-exists? config "quux2")))
+  (is (not (test-sql/verify-table-exists? config "foo")))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (not (test-sql/verify-table-exists? config "quux")))
+  (is (not (test-sql/verify-table-exists? config "quux2")))
   (core/up config 20111202110600 20120827170200)
-  (is (verify-table-exists? config "foo"))
-  (is (verify-table-exists? config "bar"))
-  (is (verify-table-exists? config "quux"))
-  (is (verify-table-exists? config "quux2")))
+  (is (test-sql/verify-table-exists? config "foo"))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (test-sql/verify-table-exists? config "quux"))
+  (is (test-sql/verify-table-exists? config "quux2")))
 
 (defn comment-out-bar-statements [sql]
   (if (re-find #"CREATE TABLE IF NOT EXISTS bar" sql)
@@ -118,15 +97,15 @@
     sql))
 
 (deftest test-migrate-with-modify-sql-fn
-  (is (not (verify-table-exists? config "foo")))
-  (is (not (verify-table-exists? config "bar")))
-  (is (not (verify-table-exists? config "quux")))
-  (is (not (verify-table-exists? config "quux2")))
+  (is (not (test-sql/verify-table-exists? config "foo")))
+  (is (not (test-sql/verify-table-exists? config "bar")))
+  (is (not (test-sql/verify-table-exists? config "quux")))
+  (is (not (test-sql/verify-table-exists? config "quux2")))
   (core/migrate (assoc config :modify-sql-fn comment-out-bar-statements))
-  (is (verify-table-exists? config "foo"))
-  (is (not (verify-table-exists? config "bar")))
-  (is (verify-table-exists? config "quux"))
-  (is (verify-table-exists? config "quux2")))
+  (is (test-sql/verify-table-exists? config "foo"))
+  (is (not (test-sql/verify-table-exists? config "bar")))
+  (is (test-sql/verify-table-exists? config "quux"))
+  (is (test-sql/verify-table-exists? config "quux2")))
 
 (deftest test-migration-table-creation-is-hooked
   (let [hook-called (atom false)]
@@ -140,20 +119,20 @@
     (is @hook-called)))
 
 (deftest test-migrate-until-just-before
-  (is (not (verify-table-exists? config "foo")))
-  (is (not (verify-table-exists? config "bar")))
-  (is (not (verify-table-exists? config "quux")))
-  (is (not (verify-table-exists? config "quux2")))
+  (is (not (test-sql/verify-table-exists? config "foo")))
+  (is (not (test-sql/verify-table-exists? config "bar")))
+  (is (not (test-sql/verify-table-exists? config "quux")))
+  (is (not (test-sql/verify-table-exists? config "quux2")))
   (core/migrate-until-just-before config 20120827170200)
-  (is (verify-table-exists? config "foo"))
-  (is (verify-table-exists? config "bar"))
-  (is (not (verify-table-exists? config "quux")))
-  (is (not (verify-table-exists? config "quux2")))
+  (is (test-sql/verify-table-exists? config "foo"))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (not (test-sql/verify-table-exists? config "quux")))
+  (is (not (test-sql/verify-table-exists? config "quux2")))
   (core/migrate config)
-  (is (verify-table-exists? config "foo"))
-  (is (verify-table-exists? config "bar"))
-  (is (verify-table-exists? config "quux"))
-  (is (verify-table-exists? config "quux2")))
+  (is (test-sql/verify-table-exists? config "foo"))
+  (is (test-sql/verify-table-exists? config "bar"))
+  (is (test-sql/verify-table-exists? config "quux"))
+  (is (test-sql/verify-table-exists? config "quux2")))
 
 (deftest test-migration-ignored-when-already-reserved
   (test-with-store
@@ -163,21 +142,21 @@
        (is (mark-reserved db migration-table-name))
        (is (not (mark-reserved db migration-table-name))))
      (testing "migrations don't run when locked"
-       (is (not (verify-table-exists? config "foo")))
+       (is (not (test-sql/verify-table-exists? config "foo")))
        (core/migrate config)
-       (is (not (verify-table-exists? config "foo"))))
+       (is (not (test-sql/verify-table-exists? config "foo"))))
      (testing "migrations run once lock is freed"
        (mark-unreserved db migration-table-name)
        (core/migrate config)
-       (is (verify-table-exists? config "foo")))
+       (is (test-sql/verify-table-exists? config "foo")))
      (testing "rollback migration isn't run when locked"
        (is (mark-reserved db migration-table-name))
        (core/down config 20111202110600)
-       (is (verify-table-exists? config "foo")))
+       (is (test-sql/verify-table-exists? config "foo")))
      (testing "rollback migration run once lock is freed"
        (mark-unreserved db migration-table-name)
        (core/down config 20111202110600)
-       (is (not (verify-table-exists? config "foo")))))))
+       (is (not (test-sql/verify-table-exists? config "foo")))))))
 
 (defn copy-dir
   [^File from ^File to]
@@ -198,18 +177,18 @@
       (copy-dir (io/file "test/migrations") migrations-dir)
       (copy-dir (io/file "test/migrations-edn") migrations-dir)
 
-      (is (not (verify-table-exists? test-config "foo")))
-      (is (not (verify-table-exists? test-config "bar")))
-      (is (not (verify-table-exists? test-config "quux")))
-      (is (not (verify-table-exists? test-config "quux2")))
+      (is (not (test-sql/verify-table-exists? test-config "foo")))
+      (is (not (test-sql/verify-table-exists? test-config "bar")))
+      (is (not (test-sql/verify-table-exists? test-config "quux")))
+      (is (not (test-sql/verify-table-exists? test-config "quux2")))
       (is (not (test-edn/test-file-exists?)))
 
       (core/migrate test-config)
 
-      (is (verify-table-exists? test-config "foo"))
-      (is (verify-table-exists? test-config "bar"))
-      (is (verify-table-exists? test-config "quux"))
-      (is (verify-table-exists? test-config "quux2"))
+      (is (test-sql/verify-table-exists? test-config "foo"))
+      (is (test-sql/verify-table-exists? test-config "bar"))
+      (is (test-sql/verify-table-exists? test-config "quux"))
+      (is (test-sql/verify-table-exists? test-config "quux2"))
       (is (test-edn/test-file-exists?))
 
       (finally
