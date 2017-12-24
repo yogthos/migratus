706dcb09c44a8a639ad7f733c58bb5bda1088dcd
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2017-12-24 01:42:54 -0500

    removed unused dependency

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
             :distribution :repo}
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/java.classpath "0.2.3"]
                  [org.clojure/java.jdbc "0.7.4"]
                  [org.clojure/tools.logging "0.4.0"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
