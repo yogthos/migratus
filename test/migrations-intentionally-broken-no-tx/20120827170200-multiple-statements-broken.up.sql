d98a3cbb7937ec119c56f44e2e7dca5651d57046
Lucas Leblow <leblowl@gmail.com>
2018-01-31 11:00:57 -0700

    add test for backing out reserved issue with tx/no-tx

---
+++ test/migrations-intentionally-broken-no-tx/20120827170200-multiple-statements-broken.up.sql
@@ -0,0 +1,16 @@
+-- :disable-transaction
+
+CREATE TABLE
+quux2
+(id bigint,
+ name varchar(255));
+
+--;;
+THIS IS MOST DEFINITELY NOT VALID SQL;
+
+--;;
+
+CREATE TABLE
+quux3
+(id bigint,
+ name varchar(255));
