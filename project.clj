8e2c464f9e98db372a65ddbd4609086f9200180c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2018-07-08 09:17:57 -0400

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
             :distribution :repo}
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/java.jdbc "0.7.4"]
-                 [org.clojure/tools.logging "0.4.0"]]
+                 [org.clojure/java.jdbc "0.7.7"]
+                 [org.clojure/tools.logging "0.4.1"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
                                   [ch.qos.logback/logback-classic "1.2.3"]
-                                  [com.h2database/h2 "1.4.196"]]}})
+                                  [com.h2database/h2 "1.4.197"]]}})
