40ffd5c27275c7b00a49ecc821fe8c07c03fc118
Dan Pomohaci <dan.pomohaci@gmail.com>
2022-06-19 17:48:11 +0300

    Multi migration dirs (#210)
    * modiy the find-migrations function to support multiple migrations dirs
    
    * remove personal dev files

--- test/migratus/test/migrations.clj
+++ test/migratus/test/migrations.clj
@@ -1,27 +1,30 @@
 (ns migratus.test.migrations
-  (:require [clojure.test :refer :all]
-            [migratus.migration.sql :as sql-mig]
-            [migratus.migrations :refer :all]
-            [migratus.utils :as utils]
-            [migratus.properties :as props]))
+  (:require
+    [clojure.test :refer [deftest is]]
+    [migratus.migration.sql :as sql-mig]
+    [migratus.migrations :as sut]
+    [migratus.properties :as props]
+    [migratus.utils :as utils]))
 
 (deftest test-parse-name
   (is (= ["20111202110600" "create-foo-table" ["up" "sql"]]
-         (parse-name "20111202110600-create-foo-table.up.sql")))
+        (sut/parse-name "20111202110600-create-foo-table.up.sql")))
   (is (= ["20111202110600" "create-foo-table" ["down" "sql"]]
-         (parse-name "20111202110600-create-foo-table.down.sql"))))
+        (sut/parse-name "20111202110600-create-foo-table.down.sql"))))
 
-(def multi-stmt-up (str "-- this is the first statement\n\n"
-                        "CREATE TABLE\nquux\n"
-                        "(id bigint,\n"
-                        " name varchar(255));\n\n"
-                        "--;;\n"
-                        "-- comment for the second statement\n\n"
-                        "CREATE TABLE quux2(id bigint, name varchar(255));\n"))
+(def multi-stmt-up
+  (str "-- this is the first statement\n\n"
+    "CREATE TABLE\nquux\n"
+    "(id bigint,\n"
+    " name varchar(255));\n\n"
+    "--;;\n"
+    "-- comment for the second statement\n\n"
+    "CREATE TABLE quux2(id bigint, name varchar(255));\n"))
 
-(def multi-stmt-down (str "DROP TABLE quux2;\n"
-                          "--;;\n"
-                          "DROP TABLE quux;\n"))
+(def multi-stmt-down
+  (str "DROP TABLE quux2;\n"
+    "--;;\n"
+    "DROP TABLE quux;\n"))
 
 (deftest test-properties
   (is (nil? (props/load-properties {})))
@@ -31,7 +34,7 @@
                  {:env ["java.home"]
                   :map {:foo "bar"
                         :baz {:bar "foo"}}}})]
-    (is (not (empty? (get props "${java.home}"))))
+    (is (seq (get props "${java.home}")))
     (is (= "bar" (get props "${foo}")))
     (is (= "foo" (get props "${baz.bar}")))))
 
@@ -51,11 +54,34 @@
            {:sql
             {:up   multi-stmt-up
              :down multi-stmt-down}}}}
-         (find-migrations "migrations" #{"init.sql"} nil)))
+        (sut/find-migrations "migrations" #{"init.sql"} nil))
+    "single migrations dir")
+  (is (= {"20220604110500"
+          {"create-foo1-table"
+           {:sql
+            {:down "DROP TABLE IF EXISTS foo1;"
+             :up "CREATE TABLE IF NOT EXISTS foo1(id bigint);"}}}
+          "20220604113000"
+          {"create-bar1-table"
+           {:sql
+            {:down "DROP TABLE IF EXISTS bar1;"
+             :up "CREATE TABLE IF NOT EXISTS bar1(id BIGINT);"}}}
+          "20220604113500"
+          {"create-bar2-table"
+           {:sql
+            {:up "CREATE TABLE IF NOT EXISTS bar2(id BIGINT);"
+             :down "DROP TABLE IF EXISTS bar2;"}}}
+          "20220604111500"
+          {"create-foo2-table"
+           {:sql
+            {:down "DROP TABLE IF EXISTS foo2;",
+             :up "CREATE TABLE IF NOT EXISTS foo2(id bigint);"}}}}
+        (sut/find-migrations ["migrations1" "migrations2"] #{} nil))
+    "multiple migrations dirs")
   (is (= {"20111202110600" {"create-foo-table" {:sql {:up   "CREATE TABLE IF NOT EXISTS foo(id bigint);\n",
                                                       :down "DROP TABLE IF EXISTS TEST_SCHEMA.foo;\n"}},
                             "create-schema"    {:sql {:up "CREATE SCHEMA TEST_SCHEMA\n"}}}}
-         (find-migrations "migrations-with-props" #{} {"${migratus.schema}" "TEST_SCHEMA"}))))
+        (sut/find-migrations "migrations-with-props" #{} {"${migratus.schema}" "TEST_SCHEMA"}))))
 
 (deftest test-find-jar-migrations
   (let [dir "migrations-in-jar"
@@ -78,24 +104,20 @@
              "multiple-statements"
              multi-stmt-up
              multi-stmt-down)}
-         (set (list-migrations {:migration-dir "migrations"})))))
+        (set (sut/list-migrations {:migration-dir "migrations"})))))
 
 (deftest test-list-migrations-bad-type
   (is (empty?
-        (list-migrations {:migration-dir "migrations-bad-type"}))))
+        (sut/list-migrations {:migration-dir "migrations-bad-type"}))))
 
 (deftest test-list-migrations-duplicate-type
   (is (thrown-with-msg?
         Exception
         #"Multiple migration types"
-        (list-migrations {:migration-dir "migrations-duplicate-type"}))))
+        (sut/list-migrations {:migration-dir "migrations-duplicate-type"}))))
 
 (deftest test-list-migrations-duplicate-name
   (is (thrown-with-msg?
         Exception
         #"Multiple migrations with id"
-        (list-migrations {:migration-dir "migrations-duplicate-name"}))))
-
-
-
-
+        (sut/list-migrations {:migration-dir "migrations-duplicate-name"}))))
