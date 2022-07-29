c31f27428a1fd374c3e8547d653120e009b72dd0
pinkfrog9 <107285219+pinkfrog9@users.noreply.github.com>
2022-07-29 21:05:37 +0800

    conceal password when logging config (#222)
    Co-authored-by: pinkfrog <pinkfrog@noname.com>

--- src/migratus/database.clj
+++ src/migratus/database.clj
@@ -283,14 +283,14 @@
     (completed-ids* @connection (migration-table-name config)))
   (migrate-up [this migration]
               (log/info "Connection is " @connection
-                        "Config is" config)
+                        "Config is" (update config :db utils/censor-password))
               (if (proto/tx? migration :up)
                 (jdbc/with-transaction [t-con (connection-or-spec @connection)]
                   (migrate-up* t-con config migration))
                 (migrate-up* (:db config) config migration)))
   (migrate-down [this migration]
                 (log/info "Connection is " @connection
-                          "Config is" config)
+                          "Config is" (update config :db utils/censor-password))
                 (if (proto/tx? migration :down)
                   (jdbc/with-transaction [t-con (connection-or-spec @connection)]
                     (migrate-down* t-con config migration))
