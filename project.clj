a141234b6c43f81f767197f867fec5ab7b5d0082
Eugen Stan <eugen.stan@netdava.com>
2022-07-31 19:48:19 +0300

    bumped up next.jdbc to 1.2.790 in lein

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
             :distribution :repo}
   :aliases {"test!" ["do" "clean," "test"]}
-  :dependencies [[com.github.seancorfield/next.jdbc "1.2.780"]
+  :dependencies [[com.github.seancorfield/next.jdbc "1.2.790"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.1.0"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
