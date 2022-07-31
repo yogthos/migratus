4b54e34f5c3df06bdd653ebdd5d40b450ab9470c
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-07-31 13:28:16 -0400

    release version 1.3.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus "1.3.8"
+(defproject migratus "1.3.9"
   :description "MIGRATE ALL THE THINGS!"
   :url "http://github.com/yogthos/migratus"
   :license {:name "Apache License, Version 2.0"
