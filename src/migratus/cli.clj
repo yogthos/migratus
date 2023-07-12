9e5f4246f14b8f964713938cd5bd5c65e192a67b
Andrei <sandre1>
2023-07-12 13:08:25 +0300

    Added tools.cli as dependency, configured logger

--- src/migratus/cli.clj
+++ src/migratus/cli.clj
@@ -1,14 +1,26 @@
 (ns migratus.cli
-  (:require [clojure.tools.cli :refer [parse-opts]]
-            [migratus.core :as migratus]
+  (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.java.io :as io]))
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.tools.logging :as log]
+            [migratus.core :as migratus])
+  (:import [java.time ZoneOffset]
+           [java.util.logging ConsoleHandler Logger SimpleFormatter]))
 
-(def cli-options
-  [[nil "--config" "Configuration file name"]
-   [nil "--until-just-before MIGRATION-ID" "Run all migrations preceding migration-id. This is useful when testing that a migration behaves as expected on fixture data. This only considers uncompleted migrations, and will not migrate down."]
-   [nil "--until-just-after MIGRATION-ID" "Migrate down all migrations after migration-id. This only considers completed migrations, and will not migrate up."]
-   [nil "--available" "List all migrations, applyed and non applyed"]
+(def global-cli-options
+  [[nil "--config NAME" "Configuration file name" :default "migratus.edn"]
+   ["-h" "--help"]])
+
+(def migrate-cli-options
+  [[nil "--until-just-before MIGRATION-ID" "Run all migrations preceding migration-id. This is useful when testing that a migration behaves as expected on fixture data. This only considers uncompleted migrations, and will not migrate down."]
+   ["-h" "--help"]])
+
+(def rollback-cli-options
+  [[nil "--until-just-after MIGRATION-ID" "Migrate down all migrations after migration-id. This only considers completed migrations, and will not migrate up."]
+   ["-h" "--help"]])
+
+(def list-cli-options
+  [[nil "--available" "List all migrations, applyed and non applyed"]
    [nil "--pending" "List pending migrations"]
    [nil "--applyed" "List applyed migrations"]
    ["-h" "--help"]])
@@ -31,72 +43,108 @@
        (str/join \newline)))
 
 (defn error-msg [errors]
-  (println "The following errors occurred while parsing your command:\n\n"
-           (str/join  \newline errors)))
+  (log/info "The following errors occurred while parsing your command:\n\n"
+            (str/join  \newline errors)))
 
 (defn no-match-message
   "No matching clause message info"
   [arguments summary]
-  (println "Migratus API does not support this action(s) : " arguments "\n\n"
-           (str/join (usage summary))))
+  (log/info "Migratus API does not support this action(s) : " arguments "\n\n"
+            (str/join (usage summary))))
 
 (defn run-migrate [cfg args]
   (let [rargs (rest args)
-        {:keys [options arguments errors summary]} (parse-opts rargs cli-options :in-order true)]
+        {:keys [options arguments errors summary]} (parse-opts rargs migrate-cli-options :in-order true)]
     (cond
       errors (error-msg errors)
-      (:until-just-before options) (do (println "calling (migrate-until-just-before cfg (rest arguments))\n
-                                             configuration is: \n" cfg "\n"
-                                                "arguments:" (rest arguments))
-                                       (migratus/migrate-until-just-before cfg (rest arguments)))
-      (empty? rargs) (do (println "calling (migrate cfg) \n configuration is: \n" cfg)
-                         (migratus/migrate cfg))
+      (:until-just-before options)
+      (do (log/info "configuration is: \n" cfg "\n"
+                    "arguments:" (rest arguments))
+          (migratus/migrate-until-just-before cfg (rest arguments)))
+      (empty? rargs)
+      (do (log/info "calling (migrate cfg) \n configuration is: \n" cfg)
+          (migratus/migrate cfg))
       :else (no-match-message rargs summary))))
 
 (defn run-rollback [cfg args]
   (let [rargs (rest args)
-        {:keys [options arguments errors summary]} (parse-opts rargs cli-options :in-order true)]
+        {:keys [options arguments errors summary]} (parse-opts rargs rollback-cli-options :in-order true)]
     (cond
+      
       errors (error-msg errors)
-      (:until-just-after options) (do (println "calling (rollback-until-just-after cfg (rest arguments)\n
-                                              configuration is: \n" cfg "\n"
-                                               "args:" (rest arguments))
-                                      (migratus/rollback-until-just-after cfg (rest arguments)))
-      (empty? rargs) (do (println "calling (rollback cfg) \n configuration is: \n" cfg)
-                         (migratus/rollback cfg))
+
+      (:until-just-after options)
+      (do (log/info "configuration is: \n" cfg "\n"
+                    "args:" (rest arguments))
+          (migratus/rollback-until-just-after cfg (rest arguments)))
+      
+      (empty? rargs)
+      (do (log/info "configuration is: \n" cfg)
+          (migratus/rollback cfg))
+      
       :else (no-match-message rargs summary))))
 
 (defn run-list [cfg args]
   (let [rargs (rest args)
-        {:keys [options _arguments errors summary]} (parse-opts rargs cli-options :in-order true)]
+        {:keys [options _arguments errors summary]} (parse-opts rargs list-cli-options :in-order true)]
     (cond
       errors (error-msg errors)
-      (:applyed options) (println "listing applyed migrations")
-      (:pending options) (do (println "listing pending migrations, configuration is: \n" cfg)
+      (:applyed options) (log/info "listing applyed migrations")
+      (:pending options) (do (log/info "listing pending migrations, configuration is: \n" cfg)
                              (migratus/pending-list cfg))
-      (:available options) (println "listing available migrations")
-      (empty? rargs) (do (println "calling (pending-list cfg) with config: \n" cfg)
+      (:available options) (log/info "listing available migrations")
+      (empty? rargs) (do (log/info "calling (pending-list cfg) with config: \n" cfg)
                          (migratus/pending-list cfg))
       :else (no-match-message rargs summary))))
 
+(defn my-custom-fn [record]
+  (let [fmt "%5$s"
+        instant (.getInstant record)
+        date (-> instant (.atZone ZoneOffset/UTC))
+        level (.getLevel record)
+        src (.getSourceClassName record)
+        msg (.getMessage record)
+        thr (.getThrown record)
+        logger (.getLoggerName record)]
+    (clojure.core/format fmt date src logger level msg thr)))
+
+(defn simple-formatter [format-fn]
+  (proxy [SimpleFormatter] []
+    (format [record]
+      (format-fn record))))
+
+(defn set-logger-format []
+  (let [main-logger (Logger/getLogger "")
+        _ (.setUseParentHandlers main-logger false)
+        handler (ConsoleHandler.)
+        formatter (simple-formatter my-custom-fn)
+        _ (.setFormatter handler formatter)
+        handlers (.getHandlers main-logger)]
+    (doseq [h handlers] (.removeHandler main-logger h))
+    (.addHandler main-logger handler)))
+
 (defn -main [& args]
-  (let [{:keys [options arguments _errors summary]} (parse-opts args cli-options :in-order true)
+  (let [{:keys [options arguments _errors summary]} (parse-opts args global-cli-options :in-order true)
         config (:config options)
         config-path (.getAbsolutePath (io/file config))
         cfg (read-string (slurp config-path))
         action (first arguments)]
-    (println "config path: " config-path)
-    (println "loaded config: " cfg)
-    (case action
-      "init" (migratus/init cfg)
-      "create" (migratus/create cfg (second arguments))
-      "migrate" (run-migrate cfg arguments)
-      "rollback" (run-rollback cfg arguments)
-      "reset" (migratus/reset cfg)
-      "up" (migratus/up cfg (rest arguments))
-      "down" (migratus/down cfg (rest arguments))
-      "list" (run-list cfg arguments)
-      (no-match-message arguments summary))))
+
+    (set-logger-format)
+    
+    (cond
+      (:help options) (usage summary)
+      (nil? (:config options)) (error-msg "No config provided \n --config [file-name]>")
+      :else (case action
+              "init" (migratus/init cfg)
+              "create" (migratus/create cfg (second arguments))
+              "migrate" (run-migrate cfg arguments)
+              "rollback" (run-rollback cfg arguments)
+              "reset" (migratus/reset cfg)
+              "up" (migratus/up cfg (rest arguments))
+              "down" (migratus/down cfg (rest arguments))
+              "list" (run-list cfg arguments)
+              (no-match-message arguments summary)))))
 
 
 
