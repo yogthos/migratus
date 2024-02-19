4b61792d4f69fda17558be4cbf638ef4d65e2b79
Eugene Pakhomov <p1himik@gmail.com>
2024-02-19 17:33:59 +0200

    Fix not being able to run migrations when nested txs are prohibited (#261)

--- test/migratus/testcontainers/postgres.clj
+++ test/migratus/testcontainers/postgres.clj
@@ -8,7 +8,7 @@
             [migratus.test.migration.sql :as test-sql]
             [migratus.core :as migratus]
             [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as rs]))
+            [next.jdbc.transaction :as jdbc-tx]))
 
 (def postgres-image (or (System/getenv "MIGRATUS_TESTCONTAINERS_POSTGRES_IMAGE") 
                         "postgres:14"))
@@ -22,39 +22,41 @@
 (deftest postgres-migrations-test
 
   (testing "Migrations are applied succesfully in PostgreSQL."
-    (let [pg-container (tc/create pg-container-spec)
-          initialized-pg-container (tc/start! pg-container)
-          meta->table-names #(into #{} (map :pg_class/table_name %))]
-      (Thread/sleep 1000)
-      (let [ds (jdbc/get-datasource {:dbtype   "postgresql"
-                                     :dbname   "postgres"
-                                     :user     "postgres"
-                                     :password "pw"
-                                     :host     (:host initialized-pg-container)
-                                     :port     (get (:mapped-ports initialized-pg-container) 5432)})
-            config {:store :database
-                    :migration-dir "migrations-postgres"
-                    :init-script "init.sql"
-                    :migration-table-name "foo_bar"
-                    :db {:datasource ds}}]
-        (is (= [] (test-sql/db-tables-and-views ds)) "db is empty before migrations")
+    (binding [;; Testing that everything works even if some wrapper code prohibits nested transactions.
+              jdbc-tx/*nested-tx* :prohibit]
+      (let [pg-container (tc/create pg-container-spec)
+            initialized-pg-container (tc/start! pg-container)
+            meta->table-names #(into #{} (map :pg_class/table_name %))]
+        (Thread/sleep 1000)
+        (let [ds (jdbc/get-datasource {:dbtype   "postgresql"
+                                       :dbname   "postgres"
+                                       :user     "postgres"
+                                       :password "pw"
+                                       :host     (:host initialized-pg-container)
+                                       :port     (get (:mapped-ports initialized-pg-container) 5432)})
+              config {:store                :database
+                      :migration-dir        "migrations-postgres"
+                      :init-script          "init.sql"
+                      :migration-table-name "foo_bar"
+                      :db                   {:datasource ds}}]
+          (is (= [] (test-sql/db-tables-and-views ds)) "db is empty before migrations")
 
-        ;; init 
-        (migratus/init config)
-        (let [db-meta (test-sql/db-tables-and-views ds)
-              table-names (meta->table-names db-meta)]
-          (is (= #{"foo"} table-names) "db is initialized"))
+          ;; init
+          (migratus/init config)
+          (let [db-meta (test-sql/db-tables-and-views ds)
+                table-names (meta->table-names db-meta)]
+            (is (= #{"foo"} table-names) "db is initialized"))
 
-        ;; migrate
-        (migratus/migrate config)
-        (let [db-meta (test-sql/db-tables-and-views ds)
-              table-names (meta->table-names db-meta) 
-              expected-tables #{"quux" "foo" "foo_bar"}]
-          (log/info "Tables are" table-names)
-          (is (= (count expected-tables) (count db-meta))
-              (str "expected table count is ok."))
+          ;; migrate
+          (migratus/migrate config)
+          (let [db-meta (test-sql/db-tables-and-views ds)
+                table-names (meta->table-names db-meta)
+                expected-tables #{"quux" "foo" "foo_bar"}]
+            (log/info "Tables are" table-names)
+            (is (= (count expected-tables) (count db-meta))
+                (str "expected table count is ok."))
 
-          (is (set/subset? expected-tables table-names)
-              "contains some tables that we expect")))
+            (is (set/subset? expected-tables table-names)
+                "contains some tables that we expect")))
 
-      (tc/stop! initialized-pg-container))))
+        (tc/stop! initialized-pg-container)))))
