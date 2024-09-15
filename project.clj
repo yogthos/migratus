2ad6dbafc769b81123a70183178ae51307a061c4
Yogthos <yogthos@gmail.com>
2024-09-15 15:38:41 -0400

    release version 1.5.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus "1.5.6"
+(defproject migratus "1.5.7"
   :description "MIGRATE ALL THE THINGS!"
   :url "http://github.com/yogthos/migratus"
   :license {:name "Apache License, Version 2.0"
